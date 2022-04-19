017eac896362b6d7d1c5157448cec216c23bc71d
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-19 09:23:22 +0300

    Create chinesezodiac.java

---
+++ chinesezodiac.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class chinesezodiac{
+    public static void main(String[] args) {
+
+        int year, remainder;
+
+        String zodiacsign = "";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Year of Birth : ");
+
+        year = input.nextInt();
+
+        remainder = year % 12;
+
+        switch(remainder){
+
+            case 0:
+
+                zodiacsign = "Monkey";
+
+                break;
+
+            case 1:
+
+                zodiacsign = "Cockerel";
+
+                break;
+
+            case 2:
+
+                zodiacsign = "Dog";
+
+                break;
+
+            case 3:
+
+                zodiacsign = "Pig";
+
+                break;
+
+            case 4:
+
+                zodiacsign = "Mouse";
+
+                break;
+
+            case 5:
+
+                zodiacsign = "Ox";
+
+                break;
+
+            case 6:
+
+                zodiacsign = "Tiger";
+
+                break;
+
+            case 7:
+
+                zodiacsign = "Rabbit";
+
+                break;
+
+            case 8:
+
+                zodiacsign = "Dragon";
+
+                break;
+
+            case 9:
+
+                zodiacsign = "Snake";
+
+                break;
+
+            case 10:
+
+                zodiacsign = "Horse";
+
+                break;
+
+            case 11:
+
+                zodiacsign = "Sheep";
+
+                break;
+
+        }
+        System.out.println("Your Chinese Zodiac Sign : " + zodiacsign);
+
+
+    }
+}
